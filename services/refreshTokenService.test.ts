ee613c0938ab22dad6d69004de46f47de421fd41
Oussema Boutaous <btaousoussema@hotmail.com>
2026-07-31 16:56:27 -0400

    Added some more tests.

--- services/refreshTokenService.test.ts
+++ services/refreshTokenService.test.ts
@@ -1,6 +1,6 @@
 import { jest, test, expect, describe } from '@jest/globals';
-import { generateRefreshTokenFromId } from './refreshTokenService.ts';
-import { insertRefreshToken, invalidateTokenForUser } from '../repo/refreshTokenModel.ts';
+import { generateRefreshTokenFromId, verifyRefreshToken } from './refreshTokenService.ts';
+import { getRefreshToken, insertRefreshToken, invalidateTokenForUser } from '../repo/refreshTokenModel.ts';
 //import * as repo from '../repo/refreshTokenModel.ts';
 import { v4 as uuid } from 'uuid';
 import type { RefreshToken } from '../types/types.ts';
@@ -20,6 +20,28 @@ describe('RefreshTokenService test', () => {
 
         (uuid as jest.Mock).mockReturnValue('mock-token');
 
+
+        const mockedInsertRefreshToken = insertRefreshToken as jest.MockedFunction<typeof insertRefreshToken>;
+        mockedInsertRefreshToken.mockResolvedValue(mockRefreshToken);
+
+         const result = await generateRefreshTokenFromId(123);
+
+        expect(invalidateTokenForUser).toHaveBeenCalledWith(123);
+
+        expect(insertRefreshToken).toHaveBeenCalledWith(
+            123,
+            'mock-token',
+            expect.any(String)
+        );
+
+        expect(result).toEqual(mockRefreshToken);
+    });
+
+    test('It should return a null', async ()=> {
+        const mockRefreshToken = null;
+
+        (uuid as jest.Mock).mockReturnValue('mock-token');
+
         const mockedInvalidatedTokenUser = invalidateTokenForUser as jest.MockedFunction<typeof invalidateTokenForUser>;
         mockedInvalidatedTokenUser.mockResolvedValue(undefined); 
 
@@ -36,6 +58,41 @@ describe('RefreshTokenService test', () => {
             expect.any(String)
         );
 
-        expect(result).toEqual(mockRefreshToken);
+        expect(result).toEqual(null);
+    });
+});
+
+describe('VerifyRefreshToken test', () => {
+    
+    test('It should return a null when the refreshToken is null', async ()=> {
+        const refreshToken = "";
+
+        const mockGetRefreshToken = getRefreshToken as jest.MockedFunction<typeof getRefreshToken>;
+        mockGetRefreshToken.mockResolvedValue(null);
+
+        const result = await verifyRefreshToken(refreshToken);
+
+        expect(result).toBe(null);
+    });
+
+    test('It should return null when a refreshToken is not active', async ()=> {
+        const mockRefreshToken:RefreshToken = {
+            id: 1,
+            user_id: 123,
+            token: 'mock-token',
+            expiry_date: 'mock-date',
+            active: false,
+        };
+
+        const mockedInvalidatedTokenUser = invalidateTokenForUser as jest.MockedFunction<typeof invalidateTokenForUser>;
+        mockedInvalidatedTokenUser.mockResolvedValue(undefined); 
+
+        const mockGetRefreshToken = getRefreshToken as jest.MockedFunction<typeof getRefreshToken>;
+        mockGetRefreshToken.mockResolvedValue(mockRefreshToken);
+
+        const result = await verifyRefreshToken("refreshToken");
+
+        expect(result).toBe(null);
+        expect(invalidateTokenForUser).toHaveBeenCalled();
     });
 });
